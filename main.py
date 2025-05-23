1f9b62bdb4c509cdc65c8726aa1428584b927b9c
saikiran1224 <knvrssaikiran@gmail.com>
2025-05-23 22:27:55 +0530

    Complete Environment creation and libraries installed

---
+++ main.py
@@ -0,0 +1,5 @@
+from trigger_crew import trigger_crew
+
+if __name__ == "__main__": 
+
+    trigger_crew() 
---
+++ trigger_crew.py
@@ -0,0 +1,20 @@
+from crewai import Crew, LLM, Agent, Task
+from crewai_tools import SerperDevTool
+
+from dotenv import load_dotenv
+load_dotenv()
+
+import json
+import os
+
+def trigger_crew():
+    
+    # Define the agent 
+    orchestration_agent = Agent(
+        role = "Orchestration Specialist",
+        goal = "Trigger the agent to perform a task when neccessary at the correct time.",
+        backstory='''You are a highly skilled orchestration specialist. You are responsible 
+        for coordinating the efforts of various agents to ensure that tasks are completed efficiently 
+        and effectively. You have a deep understanding of the capabilities and strengths of each agent, 
+        and you use this knowledge to assign tasks and manage workflows. Your goal is to ensure that all agents are working together seamlessly to achieve the desired outcomes.'''
+    )
